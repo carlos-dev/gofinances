d24d83d03805eb7658da4db50cafc93037541433
carlos-dev <carlosandre1572@gmail.com>
2022-07-04 13:30:02 -0300

    feat: styling the Interface to Select Category

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
 import theme from './src/global/styles/theme';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 SplashScreen.preventAutoHideAsync()
   .then(result => console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`))
@@ -36,7 +37,8 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* <Dashboard /> */}
-      <Register />
+      {/* <Register /> */}
+      <CategorySelect />
     </ThemeProvider>
   );
 }
